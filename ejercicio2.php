bc8a0b31dee7c4447805e8c2c627b596000289aa
mayerlyuribe <mayerlyuribe@colegioguanenta.edu.co>
2024-07-29 18:18:08 -0500

    se hicieron ejercicios

---
+++ ejercicio2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Operadores PHP</title>
+    <meta charset="UTF-8">
+    <!--Icono de la pagina -->
+    <meta rel="icon" type = "image/png" href = "/img/logo.png"/>
+    <!--Hojas de estilo de la pagina-->
+    <link rel="stylesheet" type="text/css" herf="estlos.css">
+
+</head>
+<body>
+    <h1>Sistemas<h1>
+    <h2>Guanenta<h2>
+    <hr>
+    <?php
+        $a = 10;
+        $b = 15;
+        echo ("la suma de 10 y 15 es: ", ($a+$b) , "<br/>")
+        echo ("El residuo de 15 y 10": ", ($a+$b) , "<br/>")
+        echo ("La variable \$a con incremento: ", ($a+$b) , "<br/>")
+        
+</body>
+</html>
